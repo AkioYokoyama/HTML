a0b49e58f0a8792917e1839fe8e268a780413aa1
mk3250d07295y@aol.com <Yoika>
2022-08-19 08:32:06 +0900

    add jest

--- jest/src/__test__/5/setup.test.ts
+++ jest/src/__test__/5/setup.test.ts
@@ -8,7 +8,17 @@ afterEach(() => {
 });
 
 
-test('before / after', () => {
+test('first: before / after', () => {
   expect(mock.id).toBe(13);
+  mock = {id: 13, age: 20}
+})
+
+test('second: before / after', () => {
+  expect(mock.id).toBe(13);
+  // beforeEach/afterEachはtestごとに呼ばれるため、
+  // first終了後、mockは {}になり、
+  // 再度、テスト実行時に {id: 13}となるので
+  // firstで設定した age: 20はなく、undefinedになる
+  expect(mock.name).toBeUndefined();
 })
 
